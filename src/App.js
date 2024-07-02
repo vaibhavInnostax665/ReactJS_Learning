e6794add4511f068570e797aa7f82c74f8a35eff
DESKTOP-2AFPUSU\Innostax-24-05-3 <vaibhav.tiwari@innostax.com>
2024-07-02 13:49:43 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import ComponetLearning from './pages/ComponetLearning';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>User Application</h1>
+<ComponetLearning />
+    </div>
+  );
+}
+
+export default App;
